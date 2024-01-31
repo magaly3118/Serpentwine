894dc51b5c3dee4a459d1dccedf607d313e885b0
Magaly Camacho <130792592+magaly3118@users.noreply.github.com>
2024-01-31 11:41:27 -0600

    Add color script to level.gd

--- Knit-prototype/Scripts/level.gd
+++ Knit-prototype/Scripts/level.gd
@@ -3,6 +3,14 @@ extends Node2D
 @onready var yarn = preload("res://Scenes/yarn.tscn")
 var score = 0
 
+# yarn textures
+var dark_yarn = preload("res://TestingAssets/BallYarn_ColorReshadedPink_Tiny.png")
+var light_yarn = preload("res://TestingAssets/BallYarn_Pink_Tiny.png")
+var textures = [dark_yarn, light_yarn]
+
+# texture id for body tiles
+var next_body_texture_id: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_yarn()
@@ -10,11 +18,17 @@ func _ready():
 
 func add_yarn():
 	var instance = yarn.instantiate()
-	# randmizer to pick yarn color
-	# assign corrisponding colored yarn ball asset, maybe make a function for it
 	
-	# sets the yarn ball in a random position Vector2(rand_x,rand_y)
-	instance.global_position = Vector2(randi_range(200,1100),randi_range(55,500))
+	# randomize yarn texture (color)
+	var texture_id = randi_range(0, textures.size()-1)
+	var new_texture = textures[texture_id]
+	var yarn_sprite = instance.get_child(0)
+	if yarn_sprite.texture != new_texture:
+		yarn_sprite.texture = new_texture
+	next_body_texture_id = texture_id # update id for future use 
+	
+	# sets the yarn ball in a random position 
+	instance.global_position = Vector2(randi_range(-500,500),randi_range(-250,250))
 	# in yarn, connect signal "yarn_used" to calling function spawn_yarn
 	instance.yarn_used.connect(spawn_yarn)
 	# adding a yarn node to level node
@@ -23,14 +37,14 @@ func add_yarn():
 func spawn_yarn():
 	# currently, score is just adding 5 for every ball of yarn grabbed
 	score += 5
+	# add a scarf body tile
+	$scarf.add_body_tile(next_body_texture_id)
 	# adds new yarn ball onto play area
 	add_yarn()
-	# add a scarf body tile
-	$scarf.add_body_tile()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$VBoxContainer/VBoxContainer/score_lable.text = "Rows: " + (str(score))
+	$VBoxContainer/score_lable.text = "Rows: " + (str(score))
 
 func end_game():
 	# add end game code!!!
