a2ac9b0cb0104c38e5f4353767aa6e9d6094a3ac
Nerdabelle <123666961+Nerdabelle@users.noreply.github.com>
2024-02-02 00:22:36 -0600

    button fix

--- Knit-prototype/Scripts/game_over.gd
+++ Knit-prototype/Scripts/game_over.gd
@@ -10,10 +10,11 @@ func _ready():
 func _process(delta):
 	pass
 
-
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/Menu.tscn")
 
 
 func _on_retry_pressed():
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
+
+
